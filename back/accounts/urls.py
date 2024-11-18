5e4299b66fef4a74e805e018618ab16519af4003
Riis <gunil099@gmail.com>
2024-11-18 17:13:52 +0900

    t

--- back/accounts/urls.py
+++ back/accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', views.UserRegistrationView, name='register'),
-    path('login/', views.UserLoginView, name='login'),
-    path('logout/', views.UserLogoutView, name='logout'),
+    path('register/', views.user_registration, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
