e31f66ca75b4ea52c27dcb5336654a8eba0a66ee
Riis <gunil099@gmail.com>
2024-11-21 17:07:39 +0900

    더미데이터추가

--- back/products/serializers.py
+++ back/products/serializers.py
@@ -1,14 +1,20 @@
 # products/serializers.py
 from rest_framework import serializers
-from .models import DepositProducts, DepositOptions, SavingProducts, SavingOptions
+from .models import DepositProducts, DepositOptions, SavingProducts, SavingOptions, DepositPopularity, SavingPopularity
 
 class DepositOptionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = DepositOptions
         fields = '__all__'
 
+class DepositPopularitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DepositPopularity
+        fields = '__all__'
+
 class DepositProductsSerializer(serializers.ModelSerializer):
     options = DepositOptionsSerializer(many=True, read_only=True)
+    popularity = DepositPopularitySerializer(read_only=True)
 
     class Meta:
         model = DepositProducts
@@ -19,8 +25,14 @@ class SavingOptionsSerializer(serializers.ModelSerializer):
         model = SavingOptions
         fields = '__all__'
 
+class SavingPopularitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SavingPopularity
+        fields = '__all__'
+
 class SavingProductsSerializer(serializers.ModelSerializer):
     options = SavingOptionsSerializer(many=True, read_only=True)
+    popularity = DepositPopularitySerializer(read_only=True)
 
     class Meta:
         model = SavingProducts
