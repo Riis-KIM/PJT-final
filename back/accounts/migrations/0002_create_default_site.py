becac0dcbd5155209e792b2ef3ac5b3083b5df8e
Riis-KIM <gunil099@gmail.com>
2024-11-23 21:53:31 +0900

    마이그레이션 오류 수정

--- back/accounts/migrations/0002_create_default_site.py
+++ back/accounts/migrations/0002_create_default_site.py
@@ -1,18 +1,19 @@
+# accounts/migrations/0002_create_default_site.py
 from django.db import migrations
 
-def create_site(apps, schema_editor):
+def update_site(apps, schema_editor):
     Site = apps.get_model('sites', 'Site')
-    Site.objects.create(
-        id=1,
+    Site.objects.filter(id=1).update(
         domain='localhost:5173',
         name='Seedly'
     )
 
 class Migration(migrations.Migration):
     dependencies = [
+        ('accounts', '0001_initial'),
         ('sites', '0002_alter_domain_unique'),
     ]
 
     operations = [
-        migrations.RunPython(create_site),
+        migrations.RunPython(update_site),
     ]
